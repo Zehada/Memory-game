880b85de9b4b7f0dac4e5cae9210cd93ddf40afe
Oğuzhan Öncü <oguzhan.oncu7@gmail.com>
2023-05-10 20:27:39 +0200

    Ajout du code pour avoir des paires identiques placées aléatoirement

--- script.js
+++ script.js
@@ -13,9 +13,25 @@ cards.forEach((cardFlip) => {
         if (card < 2) {
             cardFlip.classList.add('is-flipped');
         }
-        card++;
+
     })
 });
 
+// Code pour avoir des paires de cartes identiques placées aléatoirement.
 
+let cardBack = document.querySelectorAll(".flip-card-back");
+let cardList = [0, 1, 2, 3];
 
+for (i = 0; i < 2; i++) {
+
+    cardBack[cardList[0]].innerHTML += "<img src='https://picsum.photos/200/300?random=" + i + "'>";
+    cardList.splice(0, 1);
+
+    let a = Math.floor(Math.random() * cardList.length);
+    cardBack[cardList[a]].innerHTML += "<img src='https://picsum.photos/200/300?random=" + i + "'>";
+    cardList.splice(a, 1);
+    console.log(i);
+    console.log(cardList[a]);
+}
+
+console.log(cardList);
